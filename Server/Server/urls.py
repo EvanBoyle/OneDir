88141bdbd0adc4d67476d18754c5b43ae098b9f1
EvanBoyle <epb5te@virginia.edu>
2014-03-24 13:21:22 -0400

    added /Serve endpoint to demonstrate successful file serving with Django

--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -10,6 +10,8 @@ urlpatterns = patterns('',
 
     url(r'^OneDir/', 'DJServer.views.OneDir', name='OneDir'),
     url(r'^GetFiles/(?P<user>\w{0,50})/$', 'DJServer.views.GetFiles', name='GetFiles'),
+    #adjust path to reflect your directory to serve files from
+    url(r'^Serve/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/hodor/OneDir/OneDir/Server/Files'}),
     url(r'^admin/', include(admin.site.urls)),
 
 )
