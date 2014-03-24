b6a013b0e72eea19428421346a8aba4d84edc25f
EvanBoyle <epb5te@virginia.edu>
2014-03-24 13:08:46 -0400

    added example to get url parameters from link

--- Server/DJServer/views.py
+++ Server/DJServer/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import json
 # Create your views here.
 
 def OneDir(request):
     return HttpResponse("Welcome to OneDir, Beta coming soon!")
 
+def GetFiles(request, user=None):
+    response_data = {}
+    response_data['user']= user
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -7,6 +7,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Server.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
     url(r'^OneDir/', 'DJServer.views.OneDir', name='OneDir'),
+    url(r'^GetFiles/(?P<user>\w{0,50})/$', 'DJServer.views.GetFiles', name='GetFiles'),
     url(r'^admin/', include(admin.site.urls)),
+
 )
