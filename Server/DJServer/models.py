b61efcc7bbb6dc42eff89a54324392083d794ae5
EvanBoyle <epb5te@virginia.edu>
2014-03-24 14:46:21 -0400

    added file model with fk reference to user

--- Server/DJServer/models.py
+++ Server/DJServer/models.py
@@ -4,5 +4,12 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
-    passHash = models.CharField(max_lengh=256)
-    
+    passHash = models.CharField(max_length=256)
+
+class File(models.Model):
+    id = models.AutoField(primary_key=True)
+    fileName = models.CharField(max_length=500)
+    name = models.ForeignKey(User)
+    fileHash = models.CharField(max_length=256)
+    timestamp = models.TimeField()
+
