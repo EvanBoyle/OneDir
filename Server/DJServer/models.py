e3a658bb80a2574d3b9268525b98495d399b94ca
craighunter <csh7kd@virginia.edu>
2014-03-31 01:53:43 -0400

    -added proper foreign key reference to File model to reflect built-in User model -created script to flush file table, create test files (currently does not create users) -created endpoint that gets filenames for a user, e.g. http://server/GetFiles/user

--- Server/DJServer/models.py
+++ Server/DJServer/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 
 # Create your models here.
@@ -8,7 +10,6 @@ from django.db import models
 class File(models.Model):
     id = models.AutoField(primary_key=True)
     fileName = models.CharField(max_length=500)
-
+    name = models.ForeignKey(User)
     fileHash = models.CharField(max_length=256)
     timestamp = models.TimeField()
-
--- Server/DJServer/views.py
+++ Server/DJServer/views.py
@@ -6,25 +6,35 @@ from django.shortcuts import render_to_response
 from rest_framework import authtoken
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404, render
+from models import File
+from django.core import serializers
 
 import json
 # Create your views here.
 
 def OneDir(request):
     return HttpResponse("Welcome to OneDir, Beta coming soon!")
-@csrf_exempt
-def GetFiles(request, user=None):
-    response_data = {}
-    response_data['user']= user
-    return HttpResponse(json.dumps(response_data), content_type="application/json")
 
+
+# @csrf_exempt
+# def GetFiles(request, user):
+#     response_data = {}
+#     response_data['user']= user
+#     return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+@csrf_exempt
+def GetFiles(request, user):
+    query = File.objects.filter(name__username = user)
+    user_files = query.values_list('fileName', 'id')
+    newList = {}
+    for s in user_files:
+        newList[s[1]] = s[0]
+    return HttpResponse(json.dumps(newList), content_type="application/json")
 
 @api_view(['GET'])
 @csrf_exempt
 def LoggedIn(request):
-
-
-
     if request.user.is_authenticated():
         return HttpResponse("User is currently logged in. User = " + request.user.username)
     else:
