18dea26f268b283a04025bd892400d46f18b3d72
EvanBoyle <epb5te@virginia.edu>
2014-03-30 21:05:36 -0400

    added rest framework to installed apps and disabled csrf

--- Server/DJServer/views.py
+++ Server/DJServer/views.py
@@ -3,17 +3,18 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
 import json
 # Create your views here.
 
 def OneDir(request):
     return HttpResponse("Welcome to OneDir, Beta coming soon!")
-
+@csrf_exempt
 def GetFiles(request, user=None):
     response_data = {}
     response_data['user']= user
     return HttpResponse(json.dumps(response_data), content_type="application/json")
-
+@csrf_exempt
 def AuthTest(request):
     username = request.POST['username']
     password = request.POST['password']
@@ -30,7 +31,7 @@ def AuthTest(request):
     else:
         # Return an 'invalid login' error message.
         return HttpResponse("invalid login.")
-
+@csrf_exempt
 def Get_Login(request):
     if request.user.is_authenticated():
         return HttpResponse("User is currently logged in.")
