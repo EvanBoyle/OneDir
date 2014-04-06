57c80fdbe96bb0b440a2306ac1cec569650bf227
ta3fh <ta3fh@virginia.edu>
2014-04-05 23:20:22 -0400

    Got file size and dumped data to JSON file. Delayed watchdogTest.

--- Client/filemonitor.py
+++ Client/filemonitor.py
@@ -3,6 +3,7 @@ __authors__ = 'ta3fh', 'mmo7kd'
 import sys
 import time
 import os
+import json
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -11,13 +12,23 @@ class FileHandler(FileSystemEventHandler):
     Watches for events (creation, deletion, and modification of files), then prints the event type and path.
     """
     
-    dataDict = {}
+    # dataDict = {}
     def process(self, event):
-	self.dataDict[event.src_path] = event.event_type
-        print event.event_type + ": " + event.src_path
+        size = -1
+        if os.path.isfile(event.src_path):
+            if event.event_type == "created" or event.event_type == "modified":
+                filename = event.src_path.replace("\\\\", "\\")
+                print filename
+                size = os.stat(filename).st_size
+        # print event.src_path + ": " + str(size) + " " + event.event_type
+        basepath = os.path.dirname(__file__)
+        filepath = os.path.abspath(os.path.join(basepath, "..", "clientLog.json"))
+        f = open(filepath, "a")
+        f.writelines(json.dumps({'file': event.src_path, 'size': size, 'event': event.event_type}, sort_keys=True))
+        f.writelines("\n")
 
     def on_any_event(self, event):
-	self.process(event)
+        self.process(event)
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
--- Client/watchdogTest.py
+++ Client/watchdogTest.py
@@ -3,6 +3,7 @@ __author__ = 'ta3fh'
 
 import os
 import shutil
+import time
 
 def createFile(name):
     file = open(name, 'w')
@@ -15,7 +16,7 @@ def editFile(name):
     file.write("Edit \n")
 
 def moveFile(name, dir):
-    shutil.move(name, dir)
+    os.rename(name, dir)
 
 def renameFile(name, newName):
     os.rename(name, newName)
@@ -24,12 +25,22 @@ if __name__=='__main__':
 # test.txt created, test.txt modified, test.txt modified again, test_rename.txt deleted, new_test.txt created,
 # testdir created, new_test.txt deleted, new_test.txt created, testdir modified, new_test.txt deleted
     createFile("test.txt")
+    time.sleep(2)
     editFile("test.txt")
+    time.sleep(2)
     renameFile("test.txt", "test_rename.txt")
+    time.sleep(2)
     deleteFile("test_rename.txt")
-    createFile("new_test.txt")
-    if not os.path.exists("testdir"):
-        os.mkdir("testdir")
-    moveFile("new_test.txt", "testdir")
-    os.chdir("testdir")
-    deleteFile("new_test.txt")
+    time.sleep(2)
+    # if not os.path.exists("testdir"):
+    #     os.mkdir("testdir")
+    # time.sleep(2)
+    # moveFile("new_test.txt", "testdir/new_test.txt")
+    # time.sleep(2)
+    # os.chdir("testdir")
+    # time.sleep(2)
+    # editFile("new_test.txt")
+    # time.sleep(2)
+    # deleteFile("new_test.txt")
+    # time.sleep(10)
+    # shutil.rmtree("/testdir")
