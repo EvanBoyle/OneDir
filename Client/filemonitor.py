125a485af3f42eba5b43046b383a186463e13ce8
ta3fh <ta3fh@virginia.edu>
2014-04-11 03:29:46 -0400

    Modified JSON dumping to match Django timestamp. Moved and changed code from filemonitor to synchronization.

--- Client/filemonitor.py
+++ Client/filemonitor.py
@@ -1,67 +1,43 @@
-__authors__ = 'ta3fh', 'mmo7kd'
+__authors__ = 'csh7kd', 'ta3fh', 'mmo7kd'
 
 import sys
 import time
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "./Server.settings")
 import json
+import requests
+import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from views import *
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
 
 class FileHandler(FileSystemEventHandler):
     """
     Watches for events (creation, deletion, and modification of files), then prints the event type and path.
     """
-    
+
     # dataDict = {}
     def process(self, event):
         size = -1
+        time = datetime.datetime.utcnow()
+        file_time = time.strftime("%y-%m-%dT%H:%M:%S.%f")
+        file_time = file_time[:-3] + "Z" #weird formatting to match Django
         if os.path.isfile(event.src_path):
             if event.event_type == "created" or event.event_type == "modified":
                 filename = event.src_path.replace("\\\\", "\\")
-                print filename
+                #print filename
                 size = os.stat(filename).st_size
         # print event.src_path + ": " + str(size) + " " + event.event_type
         basepath = os.path.dirname(__file__)
         filepath = os.path.abspath(os.path.join(basepath, "..", "clientLog.json"))
+
         f = open(filepath, "a")
-        f.writelines(json.dumps({'file': event.src_path, 'size': size, 'event': event.event_type}, sort_keys=True))
+        f.writelines(json.dumps({'file': event.src_path, 'size': size, 'event': event.event_type, 'time': file_time}, sort_keys=True))
         f.writelines("\n")
-        check_local()
-        check_server()
+        #check_local()
+        #check_server()
 
     def on_any_event(self, event):
         self.process(event)
-
-
-def check_local():
-    filepath = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "clientLog.json"))
-    f = open(filepath, "r")
-    for line in f:
-        if line[2] == "created":
-            UploadFile(line)
-            #I know I need to change the line to a request and then send that to UploadFile, but I'm not sure how
-        if line[2] == "deleted":
-            #Here is where the code for deleting a file locally will go
-        if line[2] == "modified":
-            #Here is where the code for deleting a file locally will go
-            UploadFile(line)
-
-
-def check_server():
-    change_files = ListFiles()
-    for line in change_files:
-        if line[2] == "created":
-            UploadFile(line)
-            #I know I need to change the line to a request and then send that to UploadFile, but I'm not sure how
-        if line[2] == "deleted":
-            #Here is where the code for deleting a file will go
-        if line[2] == "modified":
-            #Here is where the code for deleting a file will go
-            UploadFile(line)
-
 
 
 if __name__ == '__main__':
---
+++ Client/synchronization.py
@@ -0,0 +1,42 @@
+__author__ = 'ta3fh', 'csh7kd'
+
+import requests
+import json
+from Server.DJServer.views import *
+
+token = ''
+username = ''
+
+def initialize(t, un):
+    global token
+    global username
+    token = t
+    username = un
+
+
+def list_files():
+    header = {}
+    header['Authorization']= 'Token '+ token
+    header['content-type']='application/json'
+    response = requests.get('http://127.0.0.1:8000/ListFiles/' + username, headers=header)
+    return response.content
+
+def check_server():
+    server_files = list_files()
+    filepath = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "clientLog.json"))
+    client_files = open(filepath, "r")
+    # For each server file, whatever the client doesn't have the client should pull from the server
+    # For each client file, whatever the server doesn't have the client should push to the server
+    for line in server_files:
+        if line[2] == "created":
+            UploadFile(line)
+            #I know I need to change the line to a request and then send that to UploadFile, but I'm not sure how
+        if line[2] == "deleted":
+            #Here is where the code for deleting a file will go
+            pass
+        if line[2] == "modified":
+            #Here is where the code for deleting a file will go
+            UploadFile(line)
+
+if __name__ == '__main__':
+    pass
