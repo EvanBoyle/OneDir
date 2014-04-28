50b3f1e3b7d25f0c601ec5b5bb564f17ae905e99
Margaret <mmo7kd@virginia.edu>
2014-04-28 10:18:57 -0400

    Fixing formatting errors.

--- Server/DJServer/adminConsole.py
+++ Server/DJServer/adminConsole.py
@@ -8,11 +8,11 @@ from models import ODFile
 def printAllFiles():
     query = ODFile.objects.filter()
     files = query.values_list()
-    print "User ID %-10s %-500s \n" % ("File Size", "File Name")
+    print "User ID  %-10s %s \n" % ("File Size", "File Name")
     fileNumber = 0
     totalSize = 0
     for f in files:
-        print "%s %-10s %-500s" (f[2], f[4], f[1])
+        print "%-9s %-10s %s" (f[2], f[4], f[1])
         fileNumber += 1
         totalSize += f[4]
     print "Total Number of files: %d \n" (fileNumber)
@@ -25,12 +25,12 @@ def printUserFiles(user):
     if query.isEmpty():
         print "No files for given user or user does not exist.\n"
     else:
-        print "File Size -500s \n" ("File Name")
+        print "File Size  File Name \n" ("File Name")
         files = query.values_list()
         for f in files:
             fileNumber += 1
             totalSize += f[4]
-            print "f[4] %-500s" (f[1])
+            print "%-11d  %d" (f[4], f[1])
         print "Total Number of files: %d \n" (fileNumber)
         print "Size of all files: %d \n" (totalSize)
 
@@ -41,8 +41,10 @@ def main():
         if action2 == "0":
             printAllFiles()
         if action2 == "1":
-            user_name = raw_input("Enter the name of the user.\n")
+            user_name = raw_input("Enter the user ID.\n")
             printUserFiles(user_name)
+    else:
+        print "Not a valid response. Please try again. \n"
 
 if __name__ == "__main__":
     main()
